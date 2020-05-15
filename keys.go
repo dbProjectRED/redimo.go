9fc87d7b83d3d22d5c85a95b7b3a60891ed2a34b
Sudhir Jonathan <sudhir.j@gmail.com>
2020-05-15 22:24:54 +0530

    Started adding general key operations

---
+++ keys.go
@@ -0,0 +1,11 @@
+package redimo
+
+func (c Client) DEL(keys ...string) (removedCount int64, err error) {
+	// TODO
+	return
+}
+
+func (c Client) EXISTS(keys ...string) (existingCount int64, err error) {
+	// TODO
+	return
+}
